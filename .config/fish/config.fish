c2c1ed3a6c01cc06c0a9a32e47d27adbdb45cfdc
Sal van Wezel <mail@salvanwezel.be>
2021-11-30 15:29:10 +0100

    docker compose

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,9 +2,9 @@ if status is-interactive
   # Commands to run in interactive sessions can go here
   
   if type -q docker-compose
-    abbr --add --global dc   'docker-compose'
-    abbr --add --global dcd  'docker-compose down'
-    abbr --add --global dcu  'docker-compose up'
-    abbr --add --global dcud 'docker-compose up -d'
+    abbr --add --global dc   'docker compose'
+    abbr --add --global dcd  'docker compose down'
+    abbr --add --global dcu  'docker compose up'
+    abbr --add --global dcud 'docker compose up -d'
   end
 end
