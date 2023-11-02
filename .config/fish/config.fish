7af8a7cac704f92099364c835c0c9250d02f2954
Sal van Wezel <mail@salvanwezel.be>
2023-11-02 12:37:24 +0100

    update changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,13 +2,17 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 
     if type -q docker
-        abbr --add --global dc 'docker compose'
-        abbr --add --global dcd 'docker compose down'
-        abbr --add --global dcu 'docker compose up'
-        abbr --add --global dcud 'docker compose up -d'
+        abbr --add dc 'docker compose'
+        abbr --add dcd 'docker compose down'
+        abbr --add dcu 'docker compose up'
+        abbr --add dcud 'docker compose up -d'
     end
 
-    abbr --add --global dg 'git --git-dir .dotfiles/ --work-tree=$HOME'
+    if type -q helix
+        abbr --add hx helix
+    end
+    
+    abbr --add dg 'git --git-dir .dotfiles/ --work-tree=$HOME'
 end
 
 if set -q KITTY_INSTALLATION_DIR
