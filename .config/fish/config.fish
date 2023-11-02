c1e60a3a5d8a2df25a0675d034c9b3cbc1663058
Sal van Wezel <mail@salvanwezel.be>
2023-11-02 12:21:50 +0100

    update changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,12 +1,21 @@
 if status is-interactive
-  # Commands to run in interactive sessions can go here
-  
-  if type -q docker
-    abbr --add --global dc   'docker compose'
-    abbr --add --global dcd  'docker compose down'
-    abbr --add --global dcu  'docker compose up'
-    abbr --add --global dcud 'docker compose up -d'
-  end
+    # Commands to run in interactive sessions can go here
 
-  abbr --add --global dg 'git --git-dir .dotfiles/ --work-tree=$HOME'
+    if type -q docker
+        abbr --add --global dc 'docker compose'
+        abbr --add --global dcd 'docker compose down'
+        abbr --add --global dcu 'docker compose up'
+        abbr --add --global dcud 'docker compose up -d'
+    end
+
+    abbr --add --global dg 'git --git-dir .dotfiles/ --work-tree=$HOME'
 end
+
+if set -q KITTY_INSTALLATION_DIR
+    set --global KITTY_SHELL_INTEGRATION enabled
+    source "$KITTY_INSTALLATION_DIR/shell-integration/fish/vendor_conf.d/kitty-shell-integration.fish"
+    set --prepend fish_complete_path "$KITTY_INSTALLATION_DIR/shell-integration/fish/vendor_completions.d"
+end
+
+# set enviroment
+export (envsubst < .env)
